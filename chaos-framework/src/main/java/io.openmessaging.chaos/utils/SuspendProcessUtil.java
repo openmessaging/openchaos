1de7729c5fbc7fc77790da9f5f64654a1fe2b270
rongtongjin <794220751@qq.com>
2020-03-20 11:50:18 +0800

    chore(checker):fix suspend fault bug

--- chaos-framework/src/main/java/io.openmessaging.chaos/utils/SuspendProcessUtil.java
+++ chaos-framework/src/main/java/io.openmessaging.chaos/utils/SuspendProcessUtil.java
@@ -7,13 +7,13 @@ public class SuspendProcessUtil {
 
     public static void suspend(String node, List<String> processNames) throws Exception {
         for (String processName : processNames) {
-            SshUtil.execCommand(node, String.format("ps ax | grep -i ‘%s’ |grep java | grep -v grep | awk '{print $1}' | xargs kill -STOP", processName));
+            SshUtil.execCommand(node, String.format("ps ax | grep -i '%s' |grep java | grep -v grep | awk '{print $1}' | xargs kill -STOP", processName));
         }
     }
 
     public static void recover(String node, List<String> processNames) throws Exception {
         for (String processName : processNames) {
-            SshUtil.execCommand(node, String.format("ps ax | grep -i ‘%s’ |grep java | grep -v grep | awk '{print $1}' | xargs kill -CONT", processName));
+            SshUtil.execCommand(node, String.format("ps ax | grep -i '%s' |grep java | grep -v grep | awk '{print $1}' | xargs kill -CONT", processName));
         }
     }
 }
