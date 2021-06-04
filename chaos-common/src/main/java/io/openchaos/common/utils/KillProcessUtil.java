bb9da988f3c733e4d0ce4a810950fcedb5f21b31
weibubli <46190832+weibubli@users.noreply.github.com>
2021-06-04 19:58:55 +0800

    Update KillProcessUtil.java

--- chaos-common/src/main/java/io/openchaos/common/utils/KillProcessUtil.java
+++ chaos-common/src/main/java/io/openchaos/common/utils/KillProcessUtil.java
@@ -28,7 +28,7 @@ public class KillProcessUtil {
 
     public static void kill(String node, String processName) throws Exception {
         log.info("Kill node {} process {} .", node, processName);
-        String pid = SshUtil.execCommandWithArgsReturnStr(node, String.format("ps ax | grep -i '%s' | grep java | grep -v grep | awk '{print $1}'", processName)).trim();
+        String pidList = SshUtil.execCommandWithArgsReturnStr(node, String.format("ps ax | grep -i '%s' | grep java | grep -v grep | awk '{print $1}'", processName)).trim();
         if (!pidList.isEmpty()) {
             String[] pids = pidList.split("\n");
             for (String pid : pids) {
