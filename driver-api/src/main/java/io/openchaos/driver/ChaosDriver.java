689c665a5721be267358a2bb3378a77ac9d0d96b
weibubli <46190832+weibubli@users.noreply.github.com>
2021-06-28 21:53:44 +0800

    Update ChaosDriver.java

--- driver-api/src/main/java/io/openchaos/driver/ChaosDriver.java
+++ driver-api/src/main/java/io/openchaos/driver/ChaosDriver.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE
  * file distributed with this work for additional information regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
@@ -12,12 +12,12 @@
  */
 
 package io.openchaos.driver;
-
+import io.openchaos.MetaDataSupport;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-public interface ChaosDriver {
+public interface ChaosDriver extends MetaDataSupport {
 
     /**
      * Driver implementation can use this method to initialize the distributed system libraries, with the provided
