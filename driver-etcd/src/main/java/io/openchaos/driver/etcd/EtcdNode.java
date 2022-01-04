113180db46d74fe4b89403dbad026123be3f3b8a
Syogin <97011557+Syogin@users.noreply.github.com>
2022-01-04 12:28:44 +0800

    Update EtcdNode.java

--- driver-etcd/src/main/java/io/openchaos/driver/etcd/EtcdNode.java
+++ driver-etcd/src/main/java/io/openchaos/driver/etcd/EtcdNode.java
@@ -1,83 +1,83 @@
-  /*
-   * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE
-   * file distributed with this work for additional information regarding copyright ownership.  The ASF licenses this file
-   * to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
-   * License.  You may obtain a copy of the License at
-   *
-   * http://www.apache.org/licenses/LICENSE-2.0
-   *
-   * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-   * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
-   * specific language governing permissions and limitations under the License.
-   */
-  package io.openchaos.driver.etcd;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 
-  import io.openchaos.common.utils.ServiceUtil;
-  import io.openchaos.driver.ChaosNode;
-  import lombok.extern.slf4j.Slf4j;
+package io.openchaos.driver.etcd;
 
-  import java.util.List;
+import io.openchaos.common.utils.ServiceUtil;
+import io.openchaos.driver.ChaosNode;
+import lombok.extern.slf4j.Slf4j;
 
-  @Slf4j
-  public class EtcdNode implements ChaosNode {
-      private static final String ETCD_PROCESS_NAME = "etcd";
+import java.util.List;
 
-      private String node;
-      private List<String> nodes;
+@Slf4j
+public class EtcdNode implements ChaosNode {
 
-      public EtcdNode(String node, List<String> nodes) {
-          this.node = node;
-          this.nodes = nodes;
-      }
+    private static final String ETCD_PROCESS_NAME = "etcd";
+    private String node;
+    private List<String> nodes;
 
-      @Override
-      public void setup() {
+    public EtcdNode(String node, List<String> nodes) {
+        this.node = node;
+        this.nodes = nodes;
+    }
 
-      }
+    @Override
+    public void setup() {
 
-      @Override
-      public void teardown() {
-          stop();
-      }
+    }
 
-      @Override
-      public void start() {
-          try {
-              ServiceUtil.start(this.node, ETCD_PROCESS_NAME);
-              log.info("start etcd({}) success", this.node);
-          } catch (Exception e) {
-              log.error("start etcd({}) failed. ", this.node, e);
-          }
-      }
+    @Override
+    public void teardown() {
+        stop();
+    }
 
-      @Override
-      public void stop() {
-          try {
-              ServiceUtil.stop(this.node, ETCD_PROCESS_NAME);
-              log.info("stop etcd({}) success", this.node);
-          } catch (Exception e) {
-              log.error("stop etcd({}) failed. ", this.node, e);
-          }
-      }
+    @Override
+    public void start() {
+        try {
+            ServiceUtil.start(this.node, ETCD_PROCESS_NAME);
+            log.info("start etcd({}) success", this.node);
+        } catch (Exception e) {
+            log.error("start etcd({}) failed. ", this.node, e);
+        }
+    }
 
-      @Override
-      public void kill() {
-          try {
-              ServiceUtil.stop(this.node, ETCD_PROCESS_NAME);
-              log.info("kill etcd({}) success", this.node);
-          } catch (Exception e) {
-              log.error("kill etcd({}) failed. ", this.node, e);
-          }
-      }
+    @Override
+    public void stop() {
+        try {
+            ServiceUtil.stop(this.node, ETCD_PROCESS_NAME);
+            log.info("stop etcd({}) success", this.node);
+        } catch (Exception e) {
+            log.error("stop etcd({}) failed. ", this.node, e);
+        }
+    }
 
+    @Override
+    public void kill() {
+        try {
+            ServiceUtil.stop(this.node, ETCD_PROCESS_NAME);
+            log.info("kill etcd({}) success", this.node);
+        } catch (Exception e) {
+            log.error("kill etcd({}) failed. ", this.node, e);
+        }
+    }
 
-      @Override
-      public void pause() {
-          stop();
-      }
+    @Override
+    public void pause() {
+        stop();
+    }
 
-      @Override
-      public void resume() {
-          start();
-      }
-  }
+    @Override
+    public void resume() {
+        start();
+    }
+}
